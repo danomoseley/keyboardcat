5d04c5b8c3c8c00e14ef341f7d34cf7fb2a7d637
U-PC\Ricardo <Ricardo@PC.(none)>
2012-12-05 22:06:56 -0500

    Multicat v1

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,46 @@
 $(document).ready(function(){
     var videos = [{weight:2, sources:['assets/meow.webm', 'assets/meow.m4v']},{weight:10, sources:['assets/Loop.webm', 'assets/Loop.m4v']},{weight:1, sources:['assets/Keyboard_Cat_Redux.webm']}];
+	var videoCount = 0;
+	var sizes = [{h:100,w:100},
+				{h:50,w:100},
+				{h:50,w:50},
+				{h:50,w:50},
+				{h:50,w:33},
+				{h:50,w:33},
+				{h:33,w:33},
+				{h:33,w:33},
+				{h:33,w:33},
+				{h:33,w:25},
+				{h:33,w:25},
+				{h:33,w:25}
+	];
+	
+    var playlist = [];
+    $.each(videos, function(k, video) {
+        for (var i=0; i<video.weight; i++) {
+            playlist.push(video.sources);
+        }
+    });
+	
+	addVideo();
+	
+	function addVideo(){
+		videoCount+=1
+		var video = $('<video autoplay>').append('');
+		
+		var random = 0; //Math.floor(Math.random() * playlist.length);		
+        var sources = playlist[random];
+        $.each(sources, function(i, source) {
+			video.append(
+				$("<source>").attr('type','video/'+source.split('.').pop()).attr('src',source)
+			);
+		});
+        $('body').append(video);
+        video.load(); 
+		$('video').css('width',sizes[videoCount-1].w+'%')
+				  .css('height',sizes[videoCount-1].h+'%');
+	}
+	
     $('video').on('dblclick', function(event){
         if (this.paused == false) {
             this.pause();
@@ -11,18 +52,10 @@ $(document).ready(function(){
         event.preventDefault();
         $(this).remove();
     });
-    $('html').on('click', function(event) {
-        if (!$(event.target).is('.praful')) {
-            $('body').append($('<img>').addClass('praful').css('top',(event.pageY-93)).css('left',(event.pageX-63)).attr('src', 'http://praful.danomoseley.com/ThePraful-Remix3.gif'));
-        }
+    $('html').on('click', function(){
+		addVideo();
     });
-    playlist = [];
-    $.each(videos, function(k, video) {
-        for (var i=0; i<video.weight; i++) {
-            playlist.push(video.sources);
-        }
-    });
-    $('video').on('ended', function(){
+    /*$('video').on('ended', function(){
         var random = Math.floor(Math.random() * playlist.length);
         $('video source').remove();
         var video = playlist[random];
@@ -33,5 +66,5 @@ $(document).ready(function(){
         });
         $('video')[0].load();
         current = video;
-    });
+    });*/
 });
